a6749729f01054dee2fd00dadb1812cf698d349e
Louis Thurston-Gibson <dev@louisthurstongibson.com>
2024-06-18 15:49:09 +0100

    Implement endpoint for getting books by author

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -23,8 +23,12 @@ public_users.get('/isbn/:isbn',function (req, res) {
   
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const author = req.params.author;
+  const filteredBooks = Object.fromEntries(
+      Object.entries(books)
+          .filter(([key, value]) => value.author === author)
+  )
+  return res.send(filteredBooks);
 });
 
 // Get all books based on title
