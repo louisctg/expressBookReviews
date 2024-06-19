f42ed74d01271ed3e3649e15eb096030554066ac
Louis Thurston-Gibson <dev@louisthurstongibson.com>
2024-06-19 16:40:08 +0100

    Reimplement get list of book to use Promise

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -1,58 +1,59 @@
-const express = require('express');
+const express = require("express");
 let books = require("./booksdb.js");
 let isValid = require("./auth_users.js").isValid;
 let users = require("./auth_users.js").users;
 const public_users = express.Router();
 
-
-public_users.post("/register", (req,res) => {
+public_users.post("/register", (req, res) => {
   const username = req.body.username;
   const password = req.body.password;
 
   if (username && password) {
-    if (!users.find(user => user.username === username)) {
-      users.push({"username":username,"password":password});
-      return res.status(200).json({message: "User successfully registred. Now you can login"});
+    if (!users.find((user) => user.username === username)) {
+      users.push({ username: username, password: password });
+      return res
+        .status(200)
+        .json({ message: "User successfully registred. Now you can login" });
     } else {
-      return res.status(409).json({message: "User already exists"});
+      return res.status(409).json({ message: "User already exists" });
     }
   }
-  return res.status(400).json({message: "Unable to register user"});
+  return res.status(400).json({ message: "Unable to register user" });
 });
 
 // Get the book list available in the shop
-public_users.get('/',function (req, res) {
-  return res.send(books);
+public_users.get("/", function (req, res) {
+  Promise.resolve(books)
+    .then((books) => res.send(books))
+    .catch((error) => res.status(500).send(error));
 });
 
 // Get book details based on ISBN
-public_users.get('/isbn/:isbn',function (req, res) {
+public_users.get("/isbn/:isbn", function (req, res) {
   const isbn = req.params.isbn;
   return res.send(books[isbn]);
 });
-  
+
 // Get book details based on author
-public_users.get('/author/:author',function (req, res) {
+public_users.get("/author/:author", function (req, res) {
   const author = req.params.author;
   const filteredBooks = Object.fromEntries(
-      Object.entries(books)
-          .filter(([key, value]) => value.author === author)
-  )
+    Object.entries(books).filter(([key, value]) => value.author === author)
+  );
   return res.send(filteredBooks);
 });
 
 // Get all books based on title
-public_users.get('/title/:title',function (req, res) {
+public_users.get("/title/:title", function (req, res) {
   const title = req.params.title;
   const filteredBooks = Object.fromEntries(
-      Object.entries(books)
-          .filter(([key, value]) => value.title === title)
-  )
+    Object.entries(books).filter(([key, value]) => value.title === title)
+  );
   return res.send(filteredBooks);
 });
 
 //  Get book review
-public_users.get('/review/:isbn',function (req, res) {
+public_users.get("/review/:isbn", function (req, res) {
   const isbn = req.params.isbn;
   const book = books[isbn];
   return res.send(book.reviews);
